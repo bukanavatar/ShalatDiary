11d4c6ee60592c1b048c351a03a63b2f70932195
rafa <azhafira222@gmail.com>
2018-04-29 19:22:39 +0700

    + menambah library material calender view + Impements onDateselected Listener dan OnMonthChangedListener

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Pages/KalenderShalatFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Pages/KalenderShalatFragment.java
@@ -1,16 +1,22 @@
 package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Pages;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.R;
 
 
-public class KalenderShalatFragment extends Fragment {
+public class KalenderShalatFragment extends Fragment implements OnDateSelectedListener, OnMonthChangedListener {
 
 
     public KalenderShalatFragment() {
@@ -27,4 +33,18 @@ public class KalenderShalatFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_kalender_shalat, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+    }
 }
