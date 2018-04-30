723d321126f994e1f62a24c6a826d99b59a46c2c
Muhammad Ilham Mubarak <muhammad_mubarak_25rpl@student.smktelkom-mlg.sch.id>
2018-04-30 13:01:51 +0700

    + Verifikasi jika user belum login maka akan masuk ke login activity

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/MainActivity.java
@@ -1,10 +1,13 @@
 package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.florent37.bubbletab.BubbleTab;
+import com.google.firebase.auth.FirebaseAuth;
 
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters.BubbleTabAdapter;
 
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     BubbleTab bubbleTab;
     ViewPager viewPager;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
         initializeView();
         viewPager.setAdapter(new BubbleTabAdapter(getSupportFragmentManager()));
         bubbleTab.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseLogin();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void firebaseLogin() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }
+        };
     }
 
     private void initializeView() {
