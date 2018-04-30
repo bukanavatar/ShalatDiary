d92824de6e447162a023af346a7cfb7bc910fa98
rafa nabila afifah <rafa_afifah_25rpl@student.smktelkom-mlg.sch.id>
2018-04-30 10:37:00 +0700

    + membuat constructor dan getter detail kalender (history) adapter

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Adapters/DetailKalenderAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Adapters/DetailKalenderAdapter.java
@@ -1,4 +1,19 @@
 package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters;
 
 public class DetailKalenderAdapter {
+    private String head;
+    private String desc;
+
+    public DetailKalenderAdapter(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
