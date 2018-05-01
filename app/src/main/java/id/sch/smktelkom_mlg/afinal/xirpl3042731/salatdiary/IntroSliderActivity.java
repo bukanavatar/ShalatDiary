0613df5c6ae3823c4d513b9226cb16be58f77031
rafa nabila afifah <rafa_afifah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-01 15:49:20 +0700

    + menetapkan agar slider hanya bisa dibuka saat pertama kali

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/IntroSliderActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/IntroSliderActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters.PrefManager;
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters.SliderAdapter;
 
 public class IntroSliderActivity extends AppCompatActivity {
@@ -20,8 +22,11 @@ public class IntroSliderActivity extends AppCompatActivity {
 
     private Button mNextBtn;
     private Button mBackBtn;
-
+    private Button mFinishBtn;
     private int mCurrentPage;
+
+    //new
+    private PrefManager prefManager;
     ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int i, float v, int i1) {
@@ -46,6 +51,13 @@ public class IntroSliderActivity extends AppCompatActivity {
                 mBackBtn.setVisibility(View.VISIBLE);
 
                 mNextBtn.setText("Finish");
+                mNextBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        launchHomeScreen();
+                        finish();
+                    }
+                });
                 mBackBtn.setText("Back");
             } else {
                 mNextBtn.setEnabled(true);
@@ -67,6 +79,13 @@ public class IntroSliderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_slider);
+//new
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
         mSlideViewPager = findViewById(R.id.sliderViewPager);
         mDotLayout = findViewById(R.id.dotsLayout);
 
@@ -92,6 +111,13 @@ public class IntroSliderActivity extends AppCompatActivity {
                 mSlideViewPager.setCurrentItem(mCurrentPage - 1);
             }
         });
+    }
+
+    //new
+    private void launchHomeScreen() {
+        prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(IntroSliderActivity.this, MainActivity.class));
+        finish();
     }
 
     public void addDotsIndicator(int position) {
