0b7bf4a9956223478aa894c992fc5c23cf3bc111
rafa nabila afifah <rafa_afifah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-01 09:00:01 +0700

    + Data Jadwal Shalat model revisi + Jadwal shalat model revisi

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Network/ApiService.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Network/ApiService.java
@@ -3,9 +3,10 @@ package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Network;
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Model.DataJadwalShalatModel;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("pray/Malang")
-    Call<DataJadwalShalatModel> getJadwalShalat();
+    Call<DataJadwalShalatModel> getJadwalShalat(@Query("key") String key);
 }
 
--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Pages/JadwalShalatFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Pages/JadwalShalatFragment.java
@@ -3,36 +3,39 @@ package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Pages;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters.JadwalShalatAdapter;
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Model.DataJadwalShalatModel;
+import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Model.JadwalShalatModel;
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Network.ApiService;
+import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Network.BaseUrlApi;
 import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 
 public class JadwalShalatFragment extends Fragment {
-    List<DataJadwalShalatModel> data = new ArrayList<>();
+    JadwalShalatAdapter jadwalShalatAdapter;
     RecyclerView mRecyclerView;
+    ProgressBar progressBar;
+    LinearLayout linearLayout;
+
     private ApiService apiService;
-    private JadwalShalatAdapter jadwalShalatAdapter;
-
-    private ArrayList<String> namaShlat = new ArrayList<>();
-
-    private String[] listShalat = {"Subuh", "Dzuhur", "Ashar", "Maghrib", "Isya"};
 
     public JadwalShalatFragment() {
         // Required empty public constructor
-    }
-
-    public static JadwalShalatFragment newInstance() {
-        return new JadwalShalatFragment();
     }
 
     @Nullable
@@ -41,10 +44,46 @@ public class JadwalShalatFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_jadwal_shalat, container, false);
     }
 
+    public static JadwalShalatFragment newInstance() {
+        return new JadwalShalatFragment();
+    }
+
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        retrofitJadwalShalat();
+    }
+
+    private void retrofitJadwalShalat() {
+        progressBar = getView().findViewById(R.id.pb_jadwalShalat);
+
+        progressBar.setVisibility(View.VISIBLE);
+        mRecyclerView = getView().findViewById(R.id.rv_jadwal_sholat);
+        apiService = BaseUrlApi.apiService();
+        apiService.getJadwalShalat("80c4419fb4b6bb8811243396d2bc455f").enqueue(new Callback<JadwalShalatModel>() {
+            @Override
+            public void onResponse(Call<JadwalShalatModel> call, Response<JadwalShalatModel> response) {
+                progressBar.setVisibility(View.GONE);
+                ArrayList<DataJadwalShalatModel> dataList = response.body().getItems();
+                jadwalShalatAdapter = new JadwalShalatAdapter(dataList);
+
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+                mRecyclerView.setLayoutManager(layoutManager);
+                mRecyclerView.setAdapter(jadwalShalatAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<JadwalShalatModel> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getActivity(), "Maaf ada kesalahan", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
