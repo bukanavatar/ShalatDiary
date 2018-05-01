cd223d349b965be2819f285f89ab5f5c4b00a549
rafa nabila afifah <rafa_afifah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-01 15:12:29 +0700

    - slider sebelum background dan icon diganti

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/Adapters/SliderAdapter.java
@@ -0,0 +1,68 @@
+package id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.Adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import id.sch.smktelkom_mlg.afinal.xirpl3042731.salatdiary.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    public int[] slide_images = {
+            R.drawable.eat_icon,
+            R.drawable.sleep_icon,
+            R.drawable.code_icon
+    };
+    public String[] slide_headings = {
+            "EAT", "SLEEP", "CODE"
+    };
+    public String[] slide_decs = {
+            "jhdkhdkskd",
+            "sjhdkhdkdk",
+            "djskdjsjds"
+    };
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == o;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = view.findViewById(R.id.slide_image);
+        TextView slideHeading = view.findViewById(R.id.slide_heading);
+        TextView slideDescription = view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_decs[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((RelativeLayout) object);
+
+    }
+}
+
