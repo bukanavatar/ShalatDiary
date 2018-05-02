7621f31bac4e817ed78923d8f868c6697e958d1f
rafa nabila afifah <rafa_afifah_25rpl@student.smktelkom-mlg.sch.id>
2018-05-02 22:24:34 +0700

    + layout login new

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/LoginActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3042731/salatdiary/LoginActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     EditText etEmail, etUsername, etPassword;
     ProgressBar progressBar;
-    Button signUp, login;
+    TextView signUp, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
